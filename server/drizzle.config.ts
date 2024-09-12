ecc3d73d8030252c66638ee8c9ee72c746d987cc
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-09-12 13:26:11 -0300

    feat: primeiro schema

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 import { env } from "./src/env";
 export default defineConfig({
-  schema: "./schema.ts",
+  schema: "./src/db/schema.ts",
   out: "./.migrations",
   dialect: "postgresql",
   dbCredentials: {

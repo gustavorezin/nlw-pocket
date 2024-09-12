03a491d895cb8239ba38e04cc51ca40c3a12dea0
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-09-12 18:48:22 -0300

    feat: rota `/goals`

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,6 +1,17 @@
 import fastify from "fastify";
+import { createGoal } from "../services/create-goal";
+import z from "zod";
 
 const app = fastify();
+
+app.post("/goals", async (request) => {
+  const createGoalSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  });
+  const body = createGoalSchema.parse(request.body);
+  await createGoal(body);
+});
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running! 🏆");

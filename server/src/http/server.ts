9d90ae323f7ad777f6cf3ecb87183be47c9670c9
Gustavo Rezin Durigon <gustavo.rezin@gmail.com>
2024-09-13 12:47:13 -0300

    feat: passando schema na rota com `type-provider`

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,17 +1,32 @@
 import fastify from "fastify";
 import { createGoal } from "../services/create-goal";
 import z from "zod";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.post("/goals", async (request) => {
-  const createGoalSchema = z.object({
-    title: z.string(),
-    desiredWeeklyFrequency: z.number().int().min(1).max(7),
-  });
-  const body = createGoalSchema.parse(request.body);
-  await createGoal(body);
-});
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.post(
+  "/goals",
+  {
+    schema: {
+      body: z.object({
+        title: z.string(),
+        desiredWeeklyFrequency: z.number(),
+      }),
+    },
+  },
+  async (request) => {
+    const { title, desiredWeeklyFrequency } = request.body;
+    await createGoal({ title, desiredWeeklyFrequency });
+  }
+);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running! 🏆");
